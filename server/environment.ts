64520a18ad2fb2062c852b8d17872775d6ecf382
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-08-09 17:36:28 +0200

    Add mongodb.

--- server/environment.ts
+++ server/environment.ts
@@ -8,12 +8,14 @@ if (process.env.NODE_ENV === 'production') {
   Object.assign(environment, production, {
     port: process.env.PORT,
     wcaOAuthClientSecret: process.env.WCA_OAUTH_SECRET,
-    jwtSecret: process.env.JWT_SECRET
+    jwtSecret: process.env.JWT_SECRET,
+    mongodbUri: process.env.MONGODB_URI
   });
 } else {
   Object.assign(environment, development, {
     port: 3000,
     wcaOAuthClientSecret: '2196f102f10f62344b8f428c62e4d8a490b5d23b750de931a91c65d3db1e3d3c',
-    jwtSecret: 'secret'
+    jwtSecret: 'secret',
+    mongodbUri: 'mongodb://localhost:27017/realtimecubing_development',
   });
 }
