911e759e8ac543f85235811b1cc64a3af49772e2
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-08-10 01:25:06 +0200

    Save rooms to the database.

--- server/sockets.ts
+++ server/sockets.ts
@@ -1,13 +1,16 @@
-const rooms = [];
+import { Room } from '../src/app/models/room.model';
 
 export function configureSockets(io, db) {
-  io.on('connection', socket => {
-    socket.emit('initialRooms', rooms);
+  io.on('connection', (socket: SocketIO.Socket) => {
+    db.collection('rooms').find().toArray().then((rooms: Room[]) => {
+      socket.emit('initialRooms', rooms);
+    });
 
-    socket.on('createRoom', room => {
-      rooms.push(room);
-      socket.join(room.name);
-      socket.broadcast.emit('newRoom', room);
+    socket.on('createRoom', (room: Room) => {
+      db.collection('rooms').insertOne(room).then(() => {
+        socket.join(room.name);
+        socket.broadcast.emit('newRoom', room);
+      });
     });
   });
 }
--- src/app/models/room.model.ts
+++ src/app/models/room.model.ts
@@ -2,6 +2,7 @@ import { User } from './user.model';
 import { WcaEvent } from './wca-event.model';
 
 export interface Room {
+  _id: string;
   name: string;
   public: boolean;
   event: WcaEvent;
