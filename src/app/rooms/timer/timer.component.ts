1d60e56d3ecb90417afd69fb773c736e4a52eff4
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-09-05 16:27:56 +0200

    Add SolveState.

--- src/app/rooms/timer/timer.component.ts
+++ src/app/rooms/timer/timer.component.ts
@@ -1,6 +1,13 @@
 import { Component, Input } from '@angular/core';
 import * as _ from 'lodash';
 
+enum SolveState {
+  Initial,
+  Inspection,
+  Solve,
+  Finished
+}
+
 @Component({
   selector: 'app-timer',
   templateUrl: './timer.component.html',
@@ -9,15 +16,19 @@ import * as _ from 'lodash';
 export class TimerComponent {
   // @Input() scramble: string;
   scramble = "D' B2 L2 U B2 D2 R2 D2 R2 U2 R B' L2 F' U B2 R B' D2 B' R";
-  inspection: number;
   startTime: number;
-  display = '';
+  display: string;
+  penalty: string;
+  state: SolveState;
   inspectionTimers: NodeJS.Timer[];
-  penalty = 'OK';
+  SolveState = SolveState;
 
-  constructor() { }
+  constructor() {
+    this.reset();
+  }
 
   startInspection(): void {
+    this.state = SolveState.Inspection;
     this.display = 'Inspection';
     this.inspectionTimers = [
       setTimeout(() => this.display = 'Inspection 8s!', 8000),
@@ -28,12 +39,14 @@ export class TimerComponent {
   }
 
   start(): void {
+    this.state = SolveState.Solve;
     this.startTime = performance.now();
     this.display = 'Solving';
     this.inspectionTimers.forEach(timer => clearTimeout(timer));
   }
 
   stop(): void {
+    this.state = SolveState.Finished;
     const timeMiliseconds = (performance.now() - this.startTime);
     const time = _.floor(timeMiliseconds / 1000, 2);
     this.display = time.toString();
@@ -41,5 +54,14 @@ export class TimerComponent {
 
   submitTime(): void {
     /* Notify about the time. */
+    this.reset();
+    this.scramble = null;
+  }
+
+  private reset(): void {
+    this.startTime = null;
+    this.display = '';
+    this.penalty = 'None';
+    this.state = SolveState.Initial;
   }
 }
