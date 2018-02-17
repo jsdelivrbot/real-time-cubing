96c0d1e4495ce66e0bafbd76691a34b0b199ba18
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-02-17 13:49:27 +0100

    Set up showing average solves.

--- src/app/rooms/averages/averages.component.ts
+++ src/app/rooms/averages/averages.component.ts
@@ -14,18 +14,18 @@ export class AveragesComponent implements DoCheck {
   @Input() solves: Solve[];
 
   displayedColumns = ['type', 'current', 'best'];
-  averages: { type: string, solvesCount: number, trimmedCount: number, current: number, best: number }[] = [];
+  averages: { current: Average, best: Average }[] = [];
   tableDataSource: BehaviorSubject<any[]> = new BehaviorSubject(this.averages);
 
   solvesDiffer: IterableDiffer<Solve>;
 
-  averageDefinitions = [
-    { type: 'Single', solvesCount: 1, trimmedCount: 0, current: null, best: Infinity },
-    { type: 'Mo3', solvesCount: 3, trimmedCount: 0, current: null, best: Infinity },
-    { type: 'Ao5', solvesCount: 5, trimmedCount: 1, current: null, best: Infinity },
-    { type: 'Ao12', solvesCount: 12, trimmedCount: 1, current: null, best: Infinity },
-    { type: 'Ao50', solvesCount: 50, trimmedCount: 3, current: null, best: Infinity },
-    { type: 'Ao100', solvesCount: 100, trimmedCount: 5, current: null, best: Infinity },
+  averageTemplates: Average[] = [
+    { type: 'Single', name: 'Single', solvesCount: 1, trimmedCount: 0, time: Infinity, solves: [] },
+    { type: 'Mo3', name: 'Mean of 3', solvesCount: 3, trimmedCount: 0, time: Infinity, solves: [] },
+    { type: 'Ao5', name: 'Average of 5', solvesCount: 5, trimmedCount: 1, time: Infinity, solves: [] },
+    { type: 'Ao12', name: 'Average of 12', solvesCount: 12, trimmedCount: 1, time: Infinity, solves: [] },
+    { type: 'Ao50', name: 'Average of 50', solvesCount: 50, trimmedCount: 3, time: Infinity, solves: [] },
+    { type: 'Ao100', name: 'Average of 100', solvesCount: 100, trimmedCount: 5, time: Infinity, solves: [] }
   ];
 
   constructor(private differs: IterableDiffers, private solveService: SolveService) {
@@ -37,7 +37,9 @@ export class AveragesComponent implements DoCheck {
     if (solveChanges) {
       solveChanges.forEachAddedItem(({ currentIndex }) => {
         const solves = this.solves.slice(0, currentIndex + 1);
-        this.averages.push(..._.filter(this.averageDefinitions, { solvesCount: solves.length }));
+        _.each(_.filter(this.averageTemplates, { solvesCount: solves.length }), averageTemplate => {
+          this.averages.push({ current: averageTemplate, best: averageTemplate });
+        });
         this.updateAverages(solves);
       });
     }
@@ -46,9 +48,23 @@ export class AveragesComponent implements DoCheck {
 
   updateAverages(solves: Solve[]): void {
     this.averages.forEach(average => {
-      const averageSolves = _.takeRight(solves, average.solvesCount);
-      average.current = this.solveService.calculateAverage(averageSolves, average.trimmedCount);
-      average.best = _.min([average.current, average.best]);
+      const currentAverage = average.current;
+      currentAverage.solves = _.takeRight(solves, currentAverage.solvesCount);
+      currentAverage.time = this.solveService.calculateAverage(currentAverage.solves, currentAverage.trimmedCount);
+      average.best = _.minBy([_.cloneDeep(currentAverage), average.best], 'time');
     });
   }
+
+  showAverage(average: Average): void {
+    console.log(average);
+  }
+}
+
+interface Average {
+  type: string,
+  name: string,
+  solvesCount: number,
+  trimmedCount: number,
+  time: number,
+  solves: Solve[]
 }
